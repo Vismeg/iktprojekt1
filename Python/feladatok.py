cc5084185b19977e782af42c24dc656b2ebc87b3
Vismeg Anett Lilla <vismegAL@KKSZKI.LOCAL>
2022-11-22 17:58:56 +0100

    3.feladat kész!

--- Python/feladatok.py
+++ Python/feladatok.py
@@ -1,3 +1,5 @@
+import random
+
 def feladat1():
     text=(input("Jó napod van?"))
 
@@ -22,5 +24,26 @@ def feladat2():
     except:
         print("nem jó értéket adtál meg.")
         
-feladat2()
+#feladat2()
+def feladat3():
+    try:
+        rnd=random.randrange(1,6)
+        print(rnd)
+        number=0
+        
+
+        while  number<1 or number>5:
+            
+            number=int(input("Kérem a számot 1 és 5 között!"))
+        if rnd>number:
+            print("Nagyobb a generált szám!")
+        elif rnd<number:
+            print("Kisebb a generált szám!")
+        else:
+            print("Eltaláltad")
+    except:
+        print("Nem jó formátum.")
+
+
+#feladat3()
 
