6b0c88b1cd590b15d9cffc80c6c15d999b8034dd
Vismeg Anett Lilla <vismegAL@KKSZKI.LOCAL>
2022-11-22 16:59:44 +0100

    1 feladat kész

---
+++ Python/feladatok.py
@@ -0,0 +1,16 @@
+def feladat1():
+    text=(input("Jó napod van?"))
+
+    if text=="Igen":
+        print("Örülök neki!")
+    elif text=="Nem":
+        print("Nem örülök neki!")
+    else:
+        print("Sajnos nem értem a válaszodat!")
+
+#feladat1()
+
+def feladat2 
+
+
+
